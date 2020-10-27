f2f19c23e850c630dad89febf2f64c30cf7c0a28
nicholas <nicholascandy91@gmail.com>
2020-10-27 22:43:47 +1300

    Set up basic personcontroller

---
+++ WishlistAPI/Services/PersonService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WishlistAPI.Models;
+
+namespace WishlistAPI.Services
+{
+    public interface IPersonService 
+    {
+        Task<IEnumerable<Person>> GetAllPersonsAsync();
+    }
+    
+    public class PersonService : IPersonService
+    {
+        private readonly ILogger<PersonService> _logger;
+
+        private readonly Container _container;
+        private readonly string _databaseId;
+        private readonly string _containerId;
+
+        public PersonService(IConfiguration configuration, ILogger<PersonService> logger, CosmosClient cosmosClient)
+        {
+            _logger = logger;
+
+            _databaseId = configuration.GetValue<string>("WishlistDbId");
+            _containerId = configuration.GetValue<string>("UsersContainerId");
+
+            _container = cosmosClient.GetContainer(_databaseId, _containerId);
+        }
+
+        public async Task<IEnumerable<Person>> GetAllPersonsAsync()
+        {
+            try
+            {
+                var persons = new List<Person>();
+
+                using (var setIterator = _container.GetItemLinqQueryable<Person>()
+                    .ToFeedIterator())
+                {
+                    while (setIterator.HasMoreResults)
+                    {
+                        var response = await setIterator.ReadNextAsync();
+
+                        _logger.LogInformation("Request charge of get operation: {0}", response.RequestCharge);
+                        _logger.LogInformation("StatusCode of operation: {0}", response.StatusCode);
+
+                        persons.AddRange(response);
+                    }
+                }
+
+                return persons;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Exception while getting persons.");
+                throw;
+            }
+        }
+    }
+}
