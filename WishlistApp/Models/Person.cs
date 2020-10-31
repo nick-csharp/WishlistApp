6d0dc458a0e2d73f13d7d279e8edb781e2788738
nicholas <nicholascandy91@gmail.com>
2020-11-01 12:46:34 +1300

    Person jsonprops

--- WishlistApp/Models/Person.cs
+++ WishlistApp/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,10 @@ namespace WishlistApp.Models
 {
     public class Person
     {
-        public int PersonId { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
     }
 }
