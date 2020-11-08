67ee896b2380b8ae97e9e994abb23d75aa2381b6
nicholas <nicholascandy91@gmail.com>
2020-11-08 14:09:13 +1300

    Implement add wishlist item

--- WishlistApp/ClientApp/src/components/Wishlist.js
+++ WishlistApp/ClientApp/src/components/Wishlist.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 import { WishlistItems } from './WishlistItems'
 import { Loading } from './Loading'
+import Emoji from './Emoji';
 
 export class Wishlist extends Component {
 
@@ -8,12 +9,58 @@ export class Wishlist extends Component {
     super(props);
     this.state = {
       wishlistData: [],
-      loading: true
+      newItemDescription: "",
+      isLoading: true
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.populateWishlistData = this.populateWishlistData.bind(this);
   }
 
   componentDidMount() {
     this.populateWishlistData();
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+
+    const itemName = e.target.name;
+    const itemValue = e.target.value;
+
+    this.setState({ [itemName]: itemValue });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    if (this.state.newItemDescription === "") {
+      return;
+    }
+
+    var personId = this.props.id;
+
+    var data = {
+      "userId": personId,
+      "description": this.state.newItemDescription
+    };
+
+    fetch(`api/person/${personId}/wishlist`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+
+      this.setState({ newItemDescription: "", isLoading: true });
+      return this.populateWishlistData();
+    })
+    .catch(error => {
+      console.error("There has been a problem with your fetch operation:", error);
+    });
   }
 
   render() {
@@ -21,15 +68,41 @@ export class Wishlist extends Component {
     return (
       <div className="container mt-4" >
         <div className="row justify-content-center">
-          <div className="col-11 col-md-10">
-            <div className="card border-top-0 rounded-0">
+          <div className="">
+            <div className="card">
               <div className="card-header">
                 <h1 className="font-weight-light text-center">{name}'s Wishlist</h1>
               </div>
 
               <div className="card-body">
+                <form className="formgroup" autoComplete="off" onSubmit={this.handleSubmit}>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="newItemDescription"
+                      placeholder="Add a wishlist item"
+                      aria-describedby="buttonAdd"
+                      value={this.state.newItemDescription}
+                      onChange={this.handleChange}
+                    />
+                    <div className="input-group-append">
+                      <button
+                        type="submit"
+                        className="btn btn-sm btn-outline-secondary emoji-button"
+                        title="Add"
+                        id="buttonAdd"
+                      >
+                        <Emoji symbol="➕" label="plus" />
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
 
-                {this.state.loading
+              <div className="card-body">
+
+                {this.state.isLoading
                   ? <Loading />
                   : <WishlistItems wishlistData={this.state.wishlistData} personId={this.props.id} />}
               </div>
@@ -43,6 +116,6 @@ export class Wishlist extends Component {
   async populateWishlistData() {
     const response = await fetch(`api/person/${this.props.id}/wishlist`);
     const data = await response.json();
-    this.setState({ wishlistData: data, loading: false });
+    this.setState({ wishlistData: data, isLoading: false });
   }
 }
