30f177fb24c45f5c6e08375064ce1c5415e5e871
nicholas <nicholascandy91@gmail.com>
2020-11-23 20:51:06 +1300

    Tidy up for demo

--- WishlistApp/ClientApp/src/App.js
+++ WishlistApp/ClientApp/src/App.js
@@ -14,38 +14,6 @@ class RootApp extends Component {
   constructor(props) {
     super(props);
     this.state = { data: "unloaded", isLoading: true };
-
-
-    this.goToDebugger = this.goToDebugger.bind(this);
-    this.callAPI = this.callAPI.bind(this);
-  }
-
-  componentDidMount() {
-    
-  }
-
-  async goToDebugger() {
-    debugger;
-    var token = await this.props.getAccessToken();
-    console.log(token);
-  }
-
-  async callAPI() {
-
-    var token = await this.props.getAccessToken();
-    debugger;
-    var headers = new Headers();
-    var bearer = "Bearer " + token.accessToken;
-    headers.append("Authorization", bearer);
-    var options = {
-      method: "GET",
-      headers: headers
-    };
-
-    fetch("api/authtest/fgsfds", options)
-      .then(function (response) {
-        debugger;
-      })
   }
 
   render() {
@@ -57,18 +25,6 @@ class RootApp extends Component {
             <Whanau path='/' />
             <Wishlist path='/person/:personId/wishlist' />
           </Router>
-
-
-          <div>
-            <span>Authorised: {this.props.isAuthenticated.toString()}</span>
-            <br />
-            <button onClick={() => this.props.login()}>Sign in</button>
-            <button onClick={() => this.props.logout()}>Sign out</button>
-            <button onClick={() => this.callAPI()}>Call API</button>
-            <br />
-            <button onClick={() => this.goToDebugger()}>Debugger</button>
-          </div>
-
         </Layout>
       </AuthContext.Provider>
     );
